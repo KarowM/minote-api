3a086c97ca00c7d63ce0ef9ab722910a041afe4b
karow <karow.foxx@gmail.com>
2021-02-20 18:57:42 +0000

    - changes resource so that it returns all notes

--- core/main.py
+++ core/main.py
@@ -4,18 +4,26 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-notes = {
-    0: 'First note',
-    1: 'Second note'
-}
+notes = [
+    {
+        'id': 0,
+        'title': 'First note',
+        'body': 'Body of first note'
+    },
+    {
+        'id': 1,
+        'title': 'Second note',
+        'body': 'Body of second note'
+    }
+]
 
 
-class Note(Resource):
-    def get(self, note_id):
-        return notes[note_id]
+class Notes(Resource):
+    def get(self):
+        return notes
 
 
-api.add_resource(Note, "/note/<int:note_id>")
+api.add_resource(Notes, '/api/notes', endpoint='all-notes')
 
 if __name__ == "__main__":
     app.run()
