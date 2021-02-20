b4bbe4beaa6d11038685ae583fc0cf38cb25fcc2
karow <karow.foxx@gmail.com>
2021-02-20 18:11:57 +0000

    - added Note resource with get endpoint that returns hello world

--- core/main.py
+++ core/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+
+class Note(Resource):
+    def get(self):
+        return "Hello World!"
+
+
+api.add_resource(Note, "/")
 
 if __name__ == "__main__":
     app.run()
