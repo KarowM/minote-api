c0bdd8f117d01c9938c2a6b69748557389cc3442
karow <karow.foxx@gmail.com>
2021-02-20 18:21:00 +0000

    - added static notes and get method endpoint to retrieve note

--- core/main.py
+++ core/main.py
@@ -4,13 +4,18 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+notes = {
+    0: 'First note',
+    1: 'Second note'
+}
+
 
 class Note(Resource):
-    def get(self):
-        return "Hello World!"
+    def get(self, note_id):
+        return notes[note_id]
 
 
-api.add_resource(Note, "/")
+api.add_resource(Note, "/note/<int:note_id>")
 
 if __name__ == "__main__":
     app.run()
