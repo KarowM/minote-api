45a68320706fb651734dec8a5de2414c66994aa5
karow <karow.foxx@gmail.com>
2021-02-27 19:00:16 +0000

    - created constants for HTTP response codes and note title/body character limits

--- core/main.py
+++ core/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 
+NOTE_BODY_CHAR_LIMIT = 250
+NOTE_TITLE_CHAR_LIMIT = 30
+
+# HTTP response codes
+OK = 200
+CREATED = 201
+NO_CONTENT = 204
+BAD_REQUEST = 400
+NOT_FOUND = 404
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -31,51 +41,51 @@ class Note(Resource):
         args = note_post_args.parse_args()
         title = args['title']
         body = args['body'] if args['body'] else ''
-        if len(title) > 30:
-            abort(400, message='Title length cannot exceed 30 characters')
-        if len(body) > 250:
-            abort(400, message='Note body length cannot exceed 250 characters')
+        if len(title) > NOTE_TITLE_CHAR_LIMIT:
+            abort(BAD_REQUEST, message='Title length cannot exceed 30 characters')
+        if len(body) > NOTE_BODY_CHAR_LIMIT:
+            abort(BAD_REQUEST, message='Note body length cannot exceed 250 characters')
         note_note = {
             'title': title,
             'body': body
         }
         notes[nextPos] = note_note
         nextPos = nextPos + 1
-        return note_note, 201
+        return note_note, CREATED
 
 
 class NoteModify(Resource):
     def patch(self, note_id):
         if note_id not in notes:
-            abort(404, message=f"Could not find note with id {note_id}")
+            abort(NOT_FOUND, message=f"Could not find note with id {note_id}")
 
         args = note_patch_args.parse_args()
         title = args['title']
         body = args['body']
         if title is None and body is None:
-            abort(400, message=f"Must provide at least title or body to patch note with id {note_id}")
+            abort(BAD_REQUEST, message=f"Must provide at least title or body to patch note with id {note_id}")
         if title:
-            if len(title) > 30:
-                abort(400, message='Title length cannot exceed 30 characters')
+            if len(title) > NOTE_TITLE_CHAR_LIMIT:
+                abort(BAD_REQUEST, message='Title length cannot exceed 30 characters')
             notes[note_id]['title'] = title
         if body:
-            if len(body) > 250:
-                abort(400, message='Body length cannot exceed 250 characters')
+            if len(body) > NOTE_BODY_CHAR_LIMIT:
+                abort(BAD_REQUEST, message='Body length cannot exceed 250 characters')
             notes[note_id]['body'] = body
 
-        return args, 200
+        return args, OK
 
     def delete(self, note_id):
         if note_id not in notes:
-            abort(404, message=f"Could not find note with id {note_id}")
+            abort(NOT_FOUND, message=f"Could not find note with id {note_id}")
 
         notes.pop(note_id)
-        return '', 204
+        return '', NO_CONTENT
 
 
 class Notes(Resource):
     def get(self):
-        return notes, 200
+        return notes, OK
 
 
 api.add_resource(Notes, '/api/notes', endpoint='all-notes')
