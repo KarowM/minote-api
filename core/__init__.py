90fa77d432bb069f06b17eabfd22b5be863d3297
karow <karow.foxx@gmail.com>
2021-04-04 17:46:01 +0100

    - mongo uri is retrieved from env variable

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,9 @@
+import os
+
 from flask import Flask
 from flask_pymongo import MongoClient
 from flask_restful import Api
 
-mongo_db = MongoClient('mongo_uri')['minote']['notes']
+mongo_db = MongoClient(os.environ.get('mongo_uri'))['minote']['notes']
 app = Flask(__name__)
 api = Api(app)
