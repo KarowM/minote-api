47a28ee95c72b19850f724966a1ab50eb35b7c4e
karow <karow.foxx@gmail.com>
2021-07-13 14:59:23 +0100

    - added test when mongodb contains note id

--- test_main.py
+++ test_main.py
@@ -23,6 +23,16 @@ class TestSum(unittest.TestCase):
 
         self.assertFalse(actual_note_existence)
 
+    @patch('core.main.ObjectId')
+    @patch('core.main.mongo_db')
+    def test_note_exists_should_return_true_if_note_id_exists(self, mock_mongo_db, mock_object_id):
+        mock_object_id.is_valid.return_value = True
+        mock_mongo_db.find_one.return_value = {}
+
+        actual_note_existence = note_exists('1')
+
+        self.assertTrue(actual_note_existence)
+
 
 if __name__ == '__main__':
     unittest.main()
