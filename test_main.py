5acc461e855f3f9926760d3eff667bd9018b7f9d
karow <karow.foxx@gmail.com>
2021-07-13 14:55:37 +0100

    - added test when mongodb does not contain a note with given id

--- test_main.py
+++ test_main.py
@@ -6,12 +6,22 @@ from core.main import note_exists
 class TestSum(unittest.TestCase):
 
     @patch('core.main.ObjectId')
-    def test_note_exists(self, mock_object_id):
+    def test_note_exists_should_return_false_if_note_id_is_invalid(self, mock_object_id):
         mock_object_id.is_valid.return_value = False
 
-        actual_note_existance = note_exists(1)
+        actual_note_existence = note_exists('1')
 
-        self.assertFalse(actual_note_existance)
+        self.assertFalse(actual_note_existence)
+
+    @patch('core.main.ObjectId')
+    @patch('core.main.mongo_db')
+    def test_note_exists_should_return_false_if_note_id_does_not_exist(self, mock_mongo_db, mock_object_id):
+        mock_object_id.is_valid.return_value = True
+        mock_mongo_db.find_one.return_value = None
+
+        actual_note_existence = note_exists('1')
+
+        self.assertFalse(actual_note_existence)
 
 
 if __name__ == '__main__':
