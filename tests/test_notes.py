ac61be8e4c51b11f168fc4ca7012623257678a50
karow <karow.foxx@gmail.com>
2021-07-17 18:12:10 +0100

    - add test for when mongo db contains one note

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,5 +1,8 @@
 import unittest
 from unittest.mock import patch
+
+from bson import ObjectId
+
 from core.resources.notes import Notes
 from core.util import constants
 
@@ -7,10 +10,25 @@ from core.util import constants
 class TestCases(unittest.TestCase):
 
     @patch('core.resources.notes.mongo_db')
-    def test_get(self, mock_mongodb):
+    def test_get_should_return_empty_note_list_with_ok_status(self, mock_mongodb):
         mock_mongodb.find.return_value = []
 
         notes = Notes()
         get_response = notes.get()
 
         self.assertEqual(([], constants.OK), get_response)
+
+    @patch('core.resources.notes.mongo_db')
+    def test_get_should_return_note_list_with_ok_status(self, mock_mongodb):
+        mock_mongodb.find.return_value = [
+            {'_id': ObjectId('60f30e236c24bc528eb8a30b'), 'title': 'new title', 'body': 'new body'}
+        ]
+
+        expected_notes_list = [
+            {'_id': '60f30e236c24bc528eb8a30b', 'title': 'new title', 'body': 'new body'}
+        ]
+
+        notes = Notes()
+        get_response = notes.get()
+
+        self.assertEqual((expected_notes_list, constants.OK), get_response)
