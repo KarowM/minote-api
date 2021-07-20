31b3b89e1eb9a5b9bd63feeb8679ff1a497ce623
karow <karow.foxx@gmail.com>
2021-07-20 11:25:24 +0100

    - added test for creating a note successfully

--- tests/test_note.py
+++ tests/test_note.py
@@ -1,10 +1,16 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import werkzeug
+from bson import ObjectId
 
 from core.resources.note import Note
 from core.util import constants
+
+
+def mocked_mongo_insert(note):
+    note['_id'] = ObjectId('111111111111111111111111')
+    return note
 
 
 class TestCases(unittest.TestCase):
@@ -38,3 +44,21 @@ class TestCases(unittest.TestCase):
 
         self.assertEqual(400, ctx.exception.code)
         self.assertEqual('Bad Request', ctx.exception.name)
+
+    @patch('core.resources.note.mongo_db')
+    def test_post_returns_created_note_and_response_code(self, mock_mongo_db):
+        mock_req_parse = Mock()
+        mock_req_parse.parse_args.return_value = {
+            'title': 'new title',
+            'body': 'new body'
+        }
+        mock_mongo_db.insert_one.side_effect = mocked_mongo_insert
+
+        Note.note_post_args = mock_req_parse
+        note = Note()
+        response = note.post()
+
+        expected_response = {'title': 'new title', 'body': 'new body', '_id': '111111111111111111111111'}
+        mock_mongo_db.insert_one.assert_called_with(expected_response)
+
+        self.assertEqual(response, (expected_response, constants.CREATED))
