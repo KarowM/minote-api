7486b4098e4f526700a89acd6f41ff05ade9ac00
karow <karow.foxx@gmail.com>
2022-02-17 21:43:46 +0000

    - added certificate location to fix SSL issue

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,10 @@
 import os
 
+import certifi
 from flask import Flask
 from flask_pymongo import MongoClient
 from flask_restful import Api
 
-mongo_db = MongoClient(os.environ.get('mongo_uri'))['minote']['notes']
+mongo_db = MongoClient(os.environ.get('mongo_uri'), ssl_ca_certs=certifi.where())['minote']['notes']
 app = Flask(__name__)
 api = Api(app)
