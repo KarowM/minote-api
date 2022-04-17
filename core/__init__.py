26a7ab30c8dbbf321a2b1ff3c94c16678e9c582d
karow <karow.foxx@gmail.com>
2022-04-17 15:02:05 +0100

    - enabled cors

--- core/__init__.py
+++ core/__init__.py
@@ -2,9 +2,11 @@ import os
 
 import certifi
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import MongoClient
 from flask_restful import Api
 
 mongo_db = MongoClient(os.environ.get('mongo_uri'), ssl_ca_certs=certifi.where())['minote']['notes']
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
